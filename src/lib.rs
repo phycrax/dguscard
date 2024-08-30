f7c14945c5af2573f56a523e472844f1c46084ef
Süha Ünüvar <87157627+phycrax@users.noreply.github.com>
2024-08-30 15:58:55 +0800

    add missing docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #![no_std]
+#![warn(missing_docs)]
 #![cfg_attr(not(doctest), doc = include_str!("../README.md"))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 mod error;
+/// Receiving data from DGUS
 pub mod rx;
+/// Sending data to DGUS
 pub mod tx;
 
 pub use error::{Error, Result};
@@ -12,18 +15,28 @@ use crc::{Crc, CRC_16_MODBUS};
 const CRC: crc::Crc<u16> = Crc::<u16>::new(&CRC_16_MODBUS);
 const HEADER: u16 = 0x5AA5;
 
-/// DGUS Commands
+/// DGUS Frame Commands
+/// 
+/// Refer to T5L DGUS2 DevGuide Section 4.2
 #[repr(u8)]
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum Command {
+    /// Write data to register
     WriteReg = 0x80,
+    /// Read data from register
     ReadReg,
+    /// Write word data to variable space with word address
     WriteWord,
+    /// Read word data from variable space
     ReadWord,
-    Curve,
+    /// Write curve buffer data
+    WriteCurve,
+    /// Unknown or not yet supported command
     Unknown,
+    /// Write double word data to variable space, double word address
     WriteDword,
+    /// Read word data from variable space
     ReadDword,
 }
 
@@ -35,7 +48,7 @@ impl From<u8> for Command {
             0x81 => ReadReg,
             0x82 => WriteWord,
             0x83 => ReadWord,
-            0x84 => Curve,
+            0x84 => WriteCurve,
             0x86 => WriteDword,
             0x87 => ReadDword,
             _ => Unknown,

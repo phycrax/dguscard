29c75555e0650a97a5228254eab2ef9acb5f2203
Süha Ünüvar <87157627+phycrax@users.noreply.github.com>
2025-05-24 03:02:08 +0800

    torequest trait and to_slice fn

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,51 @@ pub use error::{Error, Result};
 
 const CRC: Crc<u16> = Crc::<u16>::new(&CRC_16_MODBUS);
 const HEADER: u16 = 0x5AA5;
+
+use instruction::Instruction;
+use request::Request;
+use serde::Serialize;
+
+/// ToRequest trait
+/// 
+/// Users can implement this trait on their types to be able to use to_x functions
+pub trait ToRequest<T: Instruction> {
+    /// Command to be assigned to the type
+    const CMD: T;
+}
+
+/// Serialize a `T` to the given slice, with the resulting slice containing
+/// data in a serialized format.
+pub fn to_slice<'b, I: Instruction, T: ToRequest<I> + Serialize>(
+    val: &T,
+    buf: &'b mut [u8],
+    crc: bool,
+) -> Result<&'b mut [u8]> {
+    let mut request = Request::with_slice(buf, T::CMD)?;
+    request.push(val)?;
+    request.finalize(crc)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        instruction::{Word, Write},
+        to_slice, ToRequest,
+    };
+    use serde::Serialize;
+
+    #[derive(Serialize)]
+    struct Test(u16);
+    impl ToRequest<Word<Write>> for Test {
+        const CMD: Word<Write> = Word {
+            addr: 0x1234,
+            cmd: Write,
+        };
+    }
+
+    #[test]
+    pub fn slice() {
+        let mut buf = [0u8; 10];
+        let _ = to_slice(&Test(0), &mut buf, true).unwrap();
+    }
+}
